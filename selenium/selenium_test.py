e1d114e5720c74d6d288b96eaff1ed28d41e0c50
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-01-18 14:42:49 +0100

    added chrome install to pipeline

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -18,8 +18,8 @@ driver = webdriver.Chrome()
 
 options = Options()
 options.headless = True
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 # service = Service(executable_path="/usr/bin/chromedriver")
 driver = webdriver.Chrome(options=options)
 
