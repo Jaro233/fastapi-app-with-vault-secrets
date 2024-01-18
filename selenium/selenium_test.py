1c2ae7fc63d22ac9461023774b0bd4395222694c
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-01-18 12:50:40 +0100

    fixed selenium

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -8,20 +8,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from get_chrome_driver import GetChromeDriver
 
 # Replace with the path to your actual ChromeDriver
-# get_driver = GetChromeDriver()
-# get_driver.install()
+get_driver = GetChromeDriver()
+get_driver.install()
 
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 options = Options()
 options.headless = True
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-service = Service(executable_path="/usr/bin/chromedriver")
-driver = webdriver.Chrome(service=service, options=options)
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+# service = Service(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Chrome(options=options)
 
 driver.get(
     "http://localhost:8000/add-book"
