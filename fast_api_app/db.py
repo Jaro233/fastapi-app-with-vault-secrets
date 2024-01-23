d50e6a1207308dbd569ccc34c2a7ccf064ac7465
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-01-23 14:49:37 +0100

    first commit

--- fast_api_app/db.py
+++ fast_api_app/db.py
@@ -1,28 +1,35 @@
 import os
-
+import hvac
 import psycopg2
-from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
 
-load_dotenv()
-
+def get_db_credentials_from_vault():
+    # Establish a connection with Vault
+    client = hvac.Client(url=os.getenv('VAULT_ADDR'), token=os.getenv('VAULT_TOKEN'))
+    
+    
+    # Read secrets from Vault
+    read_response = client.secrets.kv.v2.read_secret_version(path='secret/db_creds', mount_point='kv')
+    return read_response['data']['data']
 
 def db_conn():
     try:
+        # Fetch credentials from Vault
+        credentials = get_db_credentials_from_vault()
+
         # Connect to an existing database
         conn = psycopg2.connect(
-            host=os.getenv("host"),
-            dbname=os.getenv("database"),
-            user=os.getenv("user"),
-            password=os.getenv("password"),
-            port=os.getenv("port"),
+            host=credentials["host"],
+            dbname=credentials["database"],
+            user=credentials["user"],
+            password=credentials["password"],
+            port=credentials["port"],
             cursor_factory=RealDictCursor,
         )
-        # Open a cursor to perform database operations
+        
         print("Database connected")
         return conn.cursor()
     except Exception as error:
         print("Connecting to database unsuccessful")
         print("Error:", error)
-        # Consider re-raising the exception or implementing a retry logic
         raise
